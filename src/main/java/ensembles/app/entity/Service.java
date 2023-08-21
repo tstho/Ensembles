642b33d91f87a2bf4d3c4db912586ff0874f937a
André Bastos <afb.bastos@gmail.com>
2023-08-21 14:29:08 +0200

    ab_displayAllServices

--- src/main/java/ensembles/app/entity/Service.java
+++ src/main/java/ensembles/app/entity/Service.java
@@ -15,11 +15,9 @@ import javax.persistence.Table;
 public class Service {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
 	private String name;
-
 	private String place;
 	
 	private Date begin;
@@ -29,54 +27,54 @@ public class Service {
 	@Enumerated(EnumType.STRING)
 	private ServiceType ServiceType ;
 	
-	private Double price; 
+	private Double price;
 	
-	private String Description; 
-
+	private String Description;
+//
+//	@ManyToMany(mappedBy = "services")
+//	    private List<Journey> journeys = new ArrayList<>();
+//	
+//	public List<Journey> getJourneys() {
+//		return journeys;
+//	}
+//
+//	public void setJourneys(List<Journey> journeys) {
+//		this.journeys = journeys;
+//	}
+	
 	public String getDescription() {
 		return Description;
 	}
-
 	public void setDescription(String description) {
 		Description = description;
 	}
-
 	public Double getPrice() {
 		return price;
 	}
-
 	public void setPrice(Double prix) {
 		price = prix;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String nom) {
 		name = nom;
 	}
-
 	public String getPlace() {
 		return place;
 	}
-
 	public void setPlace(String lieu) {
 		place = lieu;
 	}
-
 	public ServiceType getServiceType() {
 		return ServiceType;
 	}
-
 	public void setServiceType(ServiceType serviceType) {
 		ServiceType = serviceType;
 	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -84,17 +82,13 @@ public class Service {
 	public Date getBegin() {
 		return begin;
 	}
-
 	public void setBegin(Date debut) {
 		begin = debut;
 	}
-
 	public Date getEnd() {
 		return end;
 	}
-
 	public void setEnd(Date fin) {
 		end = fin;
 	}
-
 }
