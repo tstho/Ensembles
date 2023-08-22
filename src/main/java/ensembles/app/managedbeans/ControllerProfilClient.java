3e656682ad636f4e54f32e92a9746c539cd0350c
@DavDav025 <davaitcheou@gmail.com>
2023-08-22 12:28:29 +0200

    modif client

--- src/main/java/ensembles/app/managedbeans/ControllerProfilClient.java
+++ src/main/java/ensembles/app/managedbeans/ControllerProfilClient.java
@@ -1,13 +1,22 @@
 package ensembles.app.managedbeans;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 
+import ensembles.app.entity.ProfilAgence;
+import ensembles.app.entity.ProfilClient;
 import ensembles.app.service.ProfilClientService;
+import ensembles.app.viewmodels.ProfilAgenceViewModel;
 import ensembles.app.viewmodels.ProfilClientViewModel;
 
 @ManagedBean
-public class ControllerProfilClient {
+@SessionScoped
+public class ControllerProfilClient implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 
 	ProfilClientViewModel profilClientViewModel = new ProfilClientViewModel();
 	
@@ -28,4 +37,58 @@ public class ControllerProfilClient {
 		this.profilClientViewModel = profilClientViewModel;
 	}
 	
+	// Modification du profil de l'agence 
+			public String redirectToEdit(Long userId) {
+				initModifierProfilC(userId);
+				return "/client/profilClient.xhtml?faces-redirect=true";
+			}
+			
+			public void initModifierProfilC(Long userId) {
+
+				ProfilClient profilClient = profilClientService.findByUserId(userId);
+				
+				profilClientViewModel = new ProfilClientViewModel();
+				
+				// identification du client et de son adresse 
+				profilClientViewModel.setUser(profilClient.getUser());
+				profilClientViewModel.setId(profilClient.getId());
+				
+				profilClientViewModel.setAdresseId(profilClient.getAdresse().getId());
+				
+				//infos du client
+				
+				profilClientViewModel.setEmailClient(profilClient.getEmailClient());
+				profilClientViewModel.setNomClient(profilClient.getNomClient());
+				profilClientViewModel.setPrenomClient(profilClient.getPrenomClient());
+				profilClientViewModel.setTelephoneClient(profilClient.getTelephoneClient());
+				
+				// infos de l'adresse du client 
+				
+				profilClientViewModel.setCodePostal(profilClient.getAdresse().getCodePostal());
+				
+				profilClientViewModel.setComplement(profilClient.getAdresse().getComplement());
+				
+				profilClientViewModel.setNumero(profilClient.getAdresse().getNumero());
+				profilClientViewModel.setPays(profilClient.getAdresse().getPays());
+				profilClientViewModel.setVille(profilClient.getAdresse().getVille());
+				
+				profilClientViewModel.setVoie(profilClient.getAdresse().getVoie());
+				
+				
+				
+				
+				System.out.println(profilClient.toString());
+				System.out.println(profilClientViewModel.toString());
+			}
+			
+			public String modifierProfilC() {
+				
+				
+				
+				profilClientService.modifyProfilClient(profilClientViewModel);
+//					profilList = rU.findAll();
+//					resetViewModel();
+
+					return "/login/authentification.xhtml?faces-redirect=true";
+				}
 }
