c99b8bc8acf78164536a1f5157a9f2ae79610dc5
tstho <ts.salmon@proton.me>
2023-08-24 11:34:16 +0200

    TODO liaison payment website

--- src/main/java/ensembles/app/managedbeans/ControllerPayment.java
+++ src/main/java/ensembles/app/managedbeans/ControllerPayment.java
@@ -1,6 +1,5 @@
 package ensembles.app.managedbeans;
 
-
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -9,74 +8,60 @@ import javax.inject.Inject;
 import ensembles.app.service.PaymentService;
 import ensembles.app.viewmodels.PaymentViewmodel;
 
-	
-	
+@ManagedBean
+@RequestScoped
+public class ControllerPayment implements Serializable {
 
-	@ManagedBean
-	@RequestScoped
-	public class ControllerPayment implements Serializable {
-	    
-	    private static final long serialVersionUID = 1L;
-	    
-	    @Inject
-	    public PaymentViewmodel paymentViewModel;
-	    
-	    @Inject
-	    PaymentService paymentService;
+	private static final long serialVersionUID = 1L;
 
+	@Inject
+	public PaymentViewmodel paymentViewModel;
+
+	@Inject
+	PaymentService paymentService;
+
+	public void initModifierPrix() {
+		int prix1 = 30;
+		int prix2 = 90;
+		int prix3 = 500;
+
+		System.out.println("Prix 1 : " + prix1);
+		System.out.println("Prix 2 : " + prix2);
+		System.out.println("Prix 3 : " + prix3);
+
+	}
+
+	public void savePaymentWebsite() {
+		paymentService.savePayment(paymentViewModel.getCardNumber(), paymentViewModel.getCvv(),
+				paymentViewModel.getCardHolderName(), paymentViewModel.getExpirationDate(),
+				paymentViewModel.getPaymentMethod(), paymentViewModel.getPaypalAmount());
+
+		// Réinitialiser le view model
+		paymentViewModel = new PaymentViewmodel();
+
+		//TODO Créer le website en base de données
+		//TODO Attribuer le website au profilAgence
 		
-	    
-	    public void initModifierPrix() {
-	        int prix1 = 30;
-	        int prix2 = 90;
-	        int prix3 = 500;
-
-	        System.out.println("Prix 1 : " + prix1);
-	        System.out.println("Prix 2 : " + prix2);
-	        System.out.println("Prix 3 : " + prix3);
-	        
-	    }
+		String confirmationMessage = "Votre paiement a été effectué avec succès.";
+		paymentViewModel.setShowPaymentConfirmation(true, confirmationMessage);
 
 
-	        
-	    
-	    
-	    public String savePayment() {
-	        paymentService.savePayment(paymentViewModel.getCardNumber(), paymentViewModel.getCvv(),paymentViewModel.getCardHolderName(),paymentViewModel.getExpirationDate(),paymentViewModel.getPaymentMethod(),paymentViewModel.getPaypalAmount());
-	        
-	    		
-	        // Réinitialiser le view model
-	        paymentViewModel = new PaymentViewmodel();
-	        
-	        
-	        String confirmationMessage = "Votre paiement a été effectué avec succès.";
-	        paymentViewModel.setShowPaymentConfirmation(true, confirmationMessage);
+	}
 
-	        return "/agency/mainAgence.xhtml?faces-redirect=true";
-	        
-	    }
+	public PaymentViewmodel getPaymentViewModel() {
+		return paymentViewModel;
+	}
 
-	   
-        
-	    public PaymentViewmodel getPaymentViewModel() {
-	        return paymentViewModel;
-	    }
+	public void setPaymentViewModel(PaymentViewmodel paymentViewModel) {
+		this.paymentViewModel = paymentViewModel;
+	}
 
-	    public void setPaymentViewModel(PaymentViewmodel paymentViewModel) {
-	        this.paymentViewModel = paymentViewModel;
-	    }
+	public PaymentService getPaymentService() {
+		return paymentService;
+	}
 
-	    public PaymentService getPaymentService() {
-	        return paymentService;
-	    }
+	public void setPaymentService(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
 
-	    public void setPaymentService(PaymentService paymentService) {
-	        this.paymentService = paymentService;
-	    }
-
-				}
-	
-	
-	
-
-
+}
