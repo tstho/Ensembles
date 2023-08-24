4399a60ab564b301aeed9d7676f80b8e867608b5
Christophe Champare <champare@gmail.com>
2023-08-24 15:55:59 +0200

    formulaire_template

--- src/main/java/ensembles/app/viewmodels/WebSiteViewModel.java
+++ src/main/java/ensembles/app/viewmodels/WebSiteViewModel.java
@@ -1,9 +1,59 @@
 package ensembles.app.viewmodels;
 
+import ensembles.app.entity.Color;
+
 public class WebSiteViewModel {
 
+	
+	private Long id; 
+	
+	
+	private String logo;
+	private String imageBackground;
+	private Color color;
+	private String imageAboutUs;
 	private String aboutUs;
-	//private byte[] logo;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public String getImageBackground() {
+		return imageBackground;
+	}
+
+	public void setImageBackground(String imageBackground) {
+		this.imageBackground = imageBackground;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public String getImageAboutUs() {
+		return imageAboutUs;
+	}
+
+	public void setImageAboutUs(String imageAboutUs) {
+		this.imageAboutUs = imageAboutUs;
+	}
 
 	public String getAboutUs() {
 		return aboutUs;
@@ -13,12 +63,6 @@ public class WebSiteViewModel {
 		this.aboutUs = aboutUs;
 	}
 
-//	public byte[] getLogo() {
-//		return logo;
-//	}
-//
-//	public void setLogo(byte[] logo) {
-//		this.logo = logo;
-//	}
+	
 	
 }
